0ff579f584b8f64e5a99b4ae060bdedb1db75f18
not-a-genius <romagiuppo@gmail.com>
2019-03-29 12:37:29 +0100

    finished sample BootCompleted to show map images

---
+++ BootCompleted/app/src/main/java/com/sensoro/bootcompleted/DisplayImageActivity.java
@@ -0,0 +1,13 @@
+package com.sensoro.bootcompleted;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DisplayImageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_image);
+    }
+}
